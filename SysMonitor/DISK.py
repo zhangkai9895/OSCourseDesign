ef48140ad183cce23693c8db9d27248ce69a03b3
zhangkai9895 <1277215795@qq.com>
2020-04-15 16:40:03 +0800

    Disk模块基本参数获取

--- SysMonitor/DISK.py
+++ SysMonitor/DISK.py
@@ -1,50 +1,79 @@
+#-*- coding: utf-8 -*
+
+import random
+from time import sleep
+import wmi
 import matplotlib
+from PyQt5 import QtCore
+from PyQt5.QtCore import QThread, pyqtSignal, QMutex
 from PyQt5.QtWidgets import *
 from matplotlib import pyplot
 from matplotlib.axes import SubplotBase
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+from GlobalConst import *
 
 matplotlib.use("Qt5Agg")
+mutex = QMutex()
 
 
 class DISK:
 
     def __init__(self):
+        self.diskDrive_list = wmi.WMI().Win32_DiskDrive()
+        self.diskLogical_list = wmi.WMI().Win32_LogicalDisk()
+        self.diskInfo_list = wmi.WMI().Win32_PerfFormattedData_PerfDisk_LogicalDisk()
+        self.name = self.getDiskName()#磁盘 C
+        self.model = self.getModel()#磁盘信息 SSD 或者固态磁盘
+        self.readSpeed = self.getReadSpeed()#磁盘读取速率
+        self.writeSpeed = self.getWriteSpeed()#磁盘写入速率
+        self.runTime = self.getRunTime()#
+        self.capacity = self.getCapacity()#磁盘容量
+        self.isSysDisk = self.isSysDisk()#
         pass
+
+    def getDiskName(self) -> str:
+        return self.diskLogical_list[1].DeviceID
+        pass
+
+    def getReadSpeed(self) -> int:
+        return int(self.diskInfo_list[0].DiskReadBytesPerSec)/1024
+        pass
+
+    def getWriteSpeed(self) -> int:
+        return int(self.diskInfo_list[0].DiskWriteBytesPerSec)/1024
+        pass
+
+    def getRunTime(self) -> int:
+        #未找到具体方法
+        pass
+
+    def getCapacity(self) -> int:
+        return int(int(self.diskDrive_list[1].Size)/(1024*1024*1024))#C盘容量
+        pass
+
+    def isSysDisk(self) -> bool:
+        #有待商定 如果只做C盘？？？
+        pass
+
+    def getModel(self)->str:
+        return self.diskDrive_list[1].Model #SSD应为 KXG5AZNV256G TOSHIBA
 
     pass
 
 
-class DiskCanvas(FigureCanvas):
-    def __init__(self, SharedCanvas):
-        self.canvas = SharedCanvas
-        self.figure = SharedCanvas.figure  # 定义一个子图
-        print(self.figure)
-        self.axes = self.figure.add_subplot(111)
-
-    def drawCurve(self, SpeedUtilization):
-        print("磁盘的传输速度绘制正常")
-        y = SpeedUtilization
-        x = list(range(50))
-        self.axes.remove()
-        self.axes = self.figure.add_subplot(111)
-
-        # 规定xy中的数据范围
-        plt.xlim(0, 50)
-        plt.ylim(0, 100)
-        # x，y轴的名称
-        plt.xlabel("time")
-        plt.ylabel("Utilization")
-
-        # plt.axis("off")
-        # 设置x轴不显示任何东西
-        plt.xticks([])
-        plt.subplots_adjust(top=0.99, bottom=0.1, left=0.07, right=1, hspace=0, wspace=0)
-        plt.margins(0, 0)
-        self.axes.plot(x, y)  # 绘图
-        self.canvas.draw()
-
-        # 用于画布的刷新
-        # 传入一个list用来读取20个前状态的cpu占用率
+    def  UpdateDiskStatus(self):
+        self.diskDrive_list = wmi.WMI().Win32_DiskDrive()
+        self.diskLogical_list = wmi.WMI().Win32_LogicalDisk()
+        self.diskInfo_list = wmi.WMI().Win32_PerfFormattedData_PerfDisk_LogicalDisk()
+        self.readSpeed = self.getReadSpeed()  # 磁盘读取速率
+        self.writeSpeed = self.getWriteSpeed()  # 磁盘写入速率
+        self.runTime = self.getRunTime()
+#测试用
+if __name__ =="__main__":
+    disk = DISK()
+    while True:
+        disk.UpdateDiskStatus()
+        print(disk.readSpeed)
+        print(disk.writeSpeed)
